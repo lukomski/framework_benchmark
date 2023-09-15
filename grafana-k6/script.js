03150b46b1e45c7f0cd02829a86b0a0612015130
lukomski <jlukomski1@gmail.com>
2023-09-15 21:34:33 +0200

    Add k6 script

---
+++ grafana-k6/script.js
@@ -0,0 +1,14 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+const url = __ENV.url ? __ENV.url : "http://localhost:3000/member";
+
+export const options = {
+  vus: __ENV.vus ? __ENV.vus : 10,
+  duration: "30s",
+};
+
+export default function () {
+  http.get(url);
+  sleep(1);
+}

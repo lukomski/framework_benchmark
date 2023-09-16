ac86f0bdb2c427991d9dd2a9af3eb336771544c6
lukomski <jlukomski1@gmail.com>
2023-09-16 18:56:05 +0200

    Add django production run

--- django/root/settings.py
+++ django/root/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'fwbm',
         'USER': 'postgres',
-        'HOST': 'db',  # set in docker-compose.yml - comment this line to use local database (ofc create localy database)
+        'HOST': 'db',
         'PORT': 5432  # default postgres port
     }
 }
@@ -121,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

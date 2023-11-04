287c25c748f6934d4d9d095db4c21dbf00eb046a
lukomski <jlukomski1@gmail.com>
2023-11-04 18:17:36 +0100

    Django - limit fro filled-in database

--- tests/common/utils.py
+++ tests/common/utils.py
@@ -23,4 +23,38 @@ def get_incorrect_part(df) -> float:
     incorrect_part = incorrect_status.count() / (correct_status.count() + incorrect_status.count())
     return incorrect_part
 
+def durationToString(durationInSeconds) -> str:
+    seconds = durationInSeconds
+    _minute_in_seconds = 60
+    _hour_in_seconds = _minute_in_seconds * 60
+    _day_in_seconds = _hour_in_seconds * 24
+
+    days = int(seconds / _day_in_seconds)
+    seconds -= days * _day_in_seconds
+
+    hours = int(seconds / _hour_in_seconds)
+    seconds -= hours * _hour_in_seconds
+
+    minutes = int(seconds / _minute_in_seconds)
+    seconds -= minutes * _minute_in_seconds
+
+    s = ''
+    if days > 0:
+        if len(s) > 0:
+            s += ' '
+        s += f'{days}d'
+    if hours > 0:
+        if len(s) > 0:
+            s += ' '
+        s += f'{hours}h'
+    if minutes > 0:
+        if len(s) > 0:
+            s += ' '
+        s += f'{minutes}m'
+    if seconds > 0 or len(s) == 0:
+        if len(s) > 0:
+            s += ' '
+        s += f'{seconds}s'
+    return s
+
 
